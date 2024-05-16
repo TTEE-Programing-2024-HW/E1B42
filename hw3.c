58ed3ca89c81aca9766b5fe7d49bb82167cf5f2a
E1B42 <u11202242@o365.ttu.edu.tw>
2024-05-16 17:46:08 +0800

    先打到這裡有點搞不懂先看完老師的講義再繼續

--- hw3.c
+++ hw3.c
@@ -25,8 +25,62 @@ int main(void) {
     puts("22222222222222222222222222222222222222");
     system("pause");
     system("CLS");
+    int password, a = 0, n, x; 
+    while (a < 3) {
+        printf("請輸入密碼:"); 
+        scanf("%d", &password); 
+        if (password == 2024) // 密碼是2024 
+        {
+            break;
+        } else {
+            a++;
+            printf("密碼錯誤,剩下%d次機會\n", 3 - a); 
+        }
+    }
+    if (a == 3) {
+        printf("您已輸入錯3次密碼程序退出\n");
+        return 0;
+    } else {
+        printf("密碼正確\n");
+        system("pause");
+        system("CLS"); // 清除螢幕
+    } 
+    {
+	system("cls"); //清除畫螢幕 
+
+	printf("------------------------------\n");
+	printf("-       [Booking System]     -\n");
+	printf("-       a.Available seats    -\n");
+	printf("-       b.Arrange for you    -\n"); 
+	printf("-       c.Choose by yourself -\n");
+	printf("-       d.Exit               -\n");
+	printf("------------------------------\n");
+	
+	char choice;  
+
+	printf("輸入a,b,c,d:");
+	fflush(stdin);  //清除暫存區 
+	scanf("%d", &choice);
+	switch(choice) 
+	{
+		case'a':
+			system("pause");
+			system("CLS");
+		case'b':
+			printf("how many seats you need?");
+			scanf("%d,&seats");
+			 
+		
+	     
+	}
+}
+
     
     
-    return 0;
+    
+    
+    
+    
+    return 0;  
 }
 
